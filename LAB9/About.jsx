5ed9c2712cfdd22bcab3882a2006d82105afb459
SUPRITHA <supritha.23cs167@sode-edu.in>
2026-04-09 14:47:20 +0530

    Create About.jsx

---
+++ LAB9/About.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+function About(){
+    return (<div style={{ padding: "20px" }}>
+      <h1>About Page</h1>
+      <p>
+        This React application demonstrates basic routing using React Router.
+        It includes multiple pages like Home, About, and Contact.
+      </p>
+
+      <h2>Our Mission</h2>
+      <p>
+        Our goal is to help beginners understand how navigation works in React
+        applications.
+      </p>
+
+      <h2>Technologies Used</h2>
+      <ul>
+        <li>React</li>
+        <li>React Router</li>
+        <li>JavaScript (ES6)</li>
+        <li>CSS</li>
+      </ul>
+    </div>)
+} export default About;
